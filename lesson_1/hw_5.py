fe35efb2adafe01538e3d4a94fed9d043a13d07c
vltian <vltian@mail.ru>
2020-11-04 00:27:01 +0300

    [upd] первая домашка

--- lesson_1/hw_5.py
+++ lesson_1/hw_5.py
@@ -4,8 +4,8 @@
 # Выведите соответствующее сообщение. Если фирма отработала с прибылью, вычислите рентабельность выручки (соотношение прибыли к выручке).
 # Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
 
-revenue = int (input("Enter your revenue"))
-cost = int (input("Enter your costs"))
+revenue = int (input("Enter your revenue - "))
+cost = int (input("Enter your costs - "))
 res = revenue - cost
 if res < 0:
         print(f"Loss is {res}")
@@ -14,7 +14,7 @@ elif res == 0:
 else:
         print (f"Profit is {res}")
         prof_ty = res * 100 / revenue
-        print (f"Your profitability is {prof_ty} %")
-        pers = int (input("Enter personnel number"))
+        print (f"Your profitability is {round(prof_ty, 2)} %")
+        pers = int (input("Enter personnel number - "))
         prof_pers = res/pers
-        print (f"Profit per person is {prof_pers}")
+        print (f"Profit per person is {round(prof_pers, 2)}")

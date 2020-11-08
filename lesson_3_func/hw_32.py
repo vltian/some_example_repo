fb7c7c96438a7565eaabcb9405a7f5e92e55ee6e
vltian <vltian@mail.ru>
2020-11-09 01:33:27 +0300

    [upd] первая домашка

--- lesson_3_func/hw_32.py
+++ lesson_3_func/hw_32.py
@@ -7,10 +7,16 @@ def pers_file (n, f, y, c, e, t):
     str = f"Имя - {n.title()} {f.title()}, год рождения - {y}, город рождения - {c.title()}, email - {e}, телефон - {t}."
     return str
 
-n = input ("Name - ")
+# def pers_file1 (a):
+#     str = ', '.join(a)
+#     return str
+
+n = input("Name - ")
 f = input("Surname - ")
 y = input("Year - ")
 c = input("City - ")
 e = input("email - ")
 t = input("Tel. - ")
+# a = [n,f,y,c,e,t]
+# print (pers_file1(a))
 print (pers_file (n, f, y, c, e, t))

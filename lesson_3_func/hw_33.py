f3ea1697f14b114784e8ff5b5a641b6fee86b726
vltian <vltian@mail.ru>
2020-11-23 12:38:35 +0300

    третья домашка

---
+++ lesson_3_func/hw_33.py
@@ -0,0 +1,15 @@
+"""3.
+Реализовать функцию my_func(), которая принимает три позиционных аргумента, и
+возвращает сумму наибольших двух аргументов.
+"""
+
+def my_func(*args):
+    result_list = list(args)
+    sum_max = 0
+    for i in (1, 2):
+        sum_max = sum_max + max(result_list)
+        result_list.pop(result_list.index(max(result_list)))
+    return sum_max
+
+x = my_func(5000, 100, 10000)
+print (x)
